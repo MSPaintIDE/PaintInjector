3ad4e6c0643367a4e24d74307f5a1407c495d60b
RubbaBoy <adamyarris@yahoo.com>
2019-01-21 01:32:49 -0500

    Added support for reactive lower status 'tooltips'

--- PaintInjector/Program.cs
+++ PaintInjector/Program.cs
@@ -3,83 +3,147 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Windows.Automation;
+using System.Windows.Forms;
 using System.Windows.Forms.VisualStyles;
 using OpenTitlebarButtons.Native;
 using OpenTitlebarButtons.Utils;
+using Theraot.Collections;
+using Vanara.PInvoke;
 
 namespace NetFramework
 {
     internal class Program
     {
-
         [STAThread]
-        static void Main(string[] args) => new Program().Stuff();
-        
-        private EventManager _eventManager = new EventManager();
-        private Bitmap _hoverLayer = (Bitmap) Resources.ResourceManager.GetObject("Hover_Layer");
+        private static void Main(string[] args) => new Program().Stuff();
 
-        public void Stuff()
+        private readonly EventManager _eventManager = new EventManager();
+        private readonly Bitmap _hoverLayer = (Bitmap) Resources.ResourceManager.GetObject("Hover_Layer");
+        private AutomationElement window;
+        private AutomationElement statusBar;
+        private AutomationElement statusText;
+
+        private TextHoster _textHoster;
+
+        private void Stuff()
         {
-            Process process = Process.GetProcessesByName("mspaint").FirstOrDefault();
+            var process = Process.GetProcessesByName("mspaint").FirstOrDefault();
             if (process == null)
             {
                 Console.WriteLine("Cannot find any Paint process.");
                 return;
             }
 
+            window = AutomationElement.FromHandle(process.MainWindowHandle);
+            statusBar = window.FindFirst(TreeScope.Children,
+                new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.StatusBar));
+
+            statusText = statusBar.FindAll(TreeScope.Subtree,
+                new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Edit))[4];
+
             var build = AddButton(process, "Build");
-            build.Click += (sender, args) => Console.WriteLine("Build");
-            
-            var run = AddButton(process, "Run");
+            build.Click += (sender, args) => { Console.WriteLine("Build"); };
+
+            var run = AddButton(process, "Run", tooltip: "Runs the current file");
             run.Click += (sender, args) => Console.WriteLine("Run");
-            
-            var stop = AddButton(process, "Stop");
+
+            var stop = AddButton(process, "Stop", tooltip: "Stops the execution of the current program");
             stop.Click += (sender, args) => Console.WriteLine("Stop");
-            
-            var spacer = AddButton(process, "Spacer", hasHover: false);
-            
-            var commit = AddButton(process, "Commit");
+
+            AddButton(process, "Spacer", false);
+
+            var commit = AddButton(process, "Commit", tooltip: "Commits all files in the project");
             commit.Click += (sender, args) => Console.WriteLine("Commit");
-            
-            var push = AddButton(process, "Push");
+
+            var push = AddButton(process, "Push", tooltip: "Pushes all files in the project");
             push.Click += (sender, args) => Console.WriteLine("Push");
-            
-            var pull = AddButton(process, "Pull");
+
+            var pull = AddButton(process, "Pull", tooltip: "Updates the project from git");
             pull.Click += (sender, args) => Console.WriteLine("Pull");
+
+            _textHoster = AddText(process);
 
             WaitForEverything();
         }
 
-        private int lastX = 178;
+        private int _lastX = 178;
 
-        private TitlebarButtonHosterForm AddButton(Process process, string icon, bool hasHover = true, string tooltip = null)
+        private TextHoster AddText(Process process, string text = null)
         {
-            var buttonHost = new TitlebarButtonHosterForm(_eventManager, new NativeUnmanagedWindow(process.MainWindowHandle));
-            buttonHost.icon = (Bitmap) Resources.ResourceManager.GetObject(icon);
-            if (hasHover) buttonHost.hoverIcon = GenerateHover(buttonHost.icon);
-            buttonHost.XOffset = lastX;
+            var textHost = new TextHoster(_eventManager, new NativeUnmanagedWindow(process.MainWindowHandle));
+
+            var prevWidth = -1;
+            textHost.CalculateCoords += (sender, args) =>
+            {
+                var nextWidth = (int) statusText.Current.BoundingRectangle.Width;
+                if (nextWidth == prevWidth) return;
+                textHost.Width = prevWidth = nextWidth;
+                textHost.ChangeBounds(textHost.Left, textHost.Top, nextWidth, textHost.Height);
+
+                textHost.Redraw();
+
+                var statusRect = statusText.Current.BoundingRectangle;
+                var windowRect = window.Current.BoundingRectangle;
+
+                args.X += (int) (statusRect.X - windowRect.X);
+                args.Y += (int) (statusRect.Y - windowRect.Y + statusRect.Height * 0.75 - textHost.Height);
+            };
+
+            textHost.BackgroundColor = Color.FromArgb(240, 240, 240);
+            textHost.FontSize = 11;
+            textHost.SetText(text, false);
+
+            textHost.Relocate();
+
+            return textHost;
+        }
+
+        private ButtonHoster AddButton(Process process, string iconName, bool hasHover = true, string tooltip = null)
+        {
+            var buttonHost = new ButtonHoster(_eventManager, new NativeUnmanagedWindow(process.MainWindowHandle));
+
+            Bitmap icon = (Bitmap) Resources.ResourceManager.GetObject(iconName);
+            buttonHost.icon = AddBackground(icon);
+            if (hasHover) buttonHost.hoverIcon = GenerateHover(icon);
+            buttonHost.XOffset = _lastX;
             buttonHost.YOffset = 30;
-            lastX += buttonHost.Width;
+            _lastX += buttonHost.Width;
+
+            if (tooltip != null) buttonHost.Hover += (sender, args) => { _textHoster.SetText(tooltip, false); };
 
             return buttonHost;
         }
 
+        // This is needed because WindowFromPoint sees through transparency. It just emulates the normal background behind it
+        private static Bitmap AddBackground(Bitmap input)
+        {
+            var coloredLayer = new Bitmap(input.Width, input.Height);
+            var graphics = Graphics.FromImage(coloredLayer);
+            graphics.Clear(Color.White);
+            graphics.DrawLine(new Pen(Color.FromArgb(218, 219, 220)), 0, input.Height - 1, input.Width,
+                input.Height - 1);
+            graphics.DrawImage((Image) input.Clone(), new Rectangle(0, 0, input.Width, input.Height));
+            return coloredLayer;
+        }
+
         private Bitmap GenerateHover(Bitmap input)
         {
-            Bitmap hoverLayer = (Bitmap) _hoverLayer.Clone();
-            Graphics.FromImage(hoverLayer).DrawImage(input, new Rectangle(0, 0, hoverLayer.Width, hoverLayer.Height));            
+            var hoverLayer = (Bitmap) _hoverLayer.Clone();
+            Graphics.FromImage(hoverLayer).DrawImage(input, new Rectangle(0, 0, hoverLayer.Width, hoverLayer.Height));
             return hoverLayer;
         }
 
-        private void WaitForEverything()
+        private static void WaitForEverything()
         {
             // Thanks SO Ɛ> https://stackoverflow.com/a/2586635/3929546
-            
+
             ManualResetEvent quitEvent = new ManualResetEvent(false);
-        
-            Console.CancelKeyPress += (sender, eArgs) => {
+
+            Console.CancelKeyPress += (sender, eArgs) =>
+            {
                 quitEvent.Set();
                 eArgs.Cancel = true;
             };
